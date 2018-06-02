a53a4d337796ca978a37d7c3f3dbe21fafd3aba6
MCPolichette <mark@polichette.com>
2018-06-02 17:43:18 -0600

    most basic framework

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,10 @@
 var whatever = "run"
+
+// secondary tag navigation / hides/displays cards based on navigation clicking
+function hideDisplay(tab) {
+    $(".work-box-card").hide()
+    $(tab).show();
+}
+$(".recipe-tab").on("click", function () { hideDisplay("#recipe-card") });
+$(".log-in-tab").on("click", function () { hideDisplay("#log-in-card") });
+$(".shopping-list-tab").on("click", function () { hideDisplay("#shopping-list-card") });
